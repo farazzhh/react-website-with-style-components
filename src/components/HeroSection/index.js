ea56ab3fcdb14bec31d15663428590f701ffb836
farazzhh <havaee.faraz@gmail.com>
2021-06-19 20:43:43 +0430

    final edit

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Video from "../../videos/video.mp4";
-import { Button } from '../ButtonElement'
+import { Button } from "../ButtonElement";
 import {
   HeroContainer,
   HeroBg,
@@ -14,23 +14,32 @@ import {
 } from "./HeroElements";
 
 const HeroSection = () => {
-  const [hover, setHover] = useState(false)
+  const [hover, setHover] = useState(false);
 
   const onHover = () => {
-    setHover(!hover)
-  }
+    setHover(!hover);
+  };
   return (
-    <HeroContainer>
+    <HeroContainer id="home">
       <HeroBg>
         <VideoBg autoPlay loop muted src={Video} type="video/mp4"></VideoBg>
       </HeroBg>
       <HeroContent>
         <HeroH1>The World is on Yours Control</HeroH1>
-        <HeroP>
-          own yours to control others
-        </HeroP>
+        <HeroP>own yours to control others</HeroP>
         <HeroBtnWrapper>
-          <Button to="signup" onMouseEnter={onHover} onMouseLeave={onHover} primary='false' dark='true'  >
+          <Button
+            to="signup"
+            onMouseEnter={onHover}
+            onMouseLeave={onHover}
+            primary="false"
+            dark="true"
+            smooth={true}
+            duration={500}
+            spy={true}
+            exact="true"
+            offset={-80}
+          >
             Get started {hover ? <ArrowForward /> : <ArrowRight />}
           </Button>
         </HeroBtnWrapper>
